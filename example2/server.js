6a5c37febdb9c040450621d9f4d0985bf47f0144
Thiagtao da Silva Adriano <tadriano@band.com.br>
2018-05-10 09:14:10 -0300

    example 2

--- example2/server.js
+++ example2/server.js
@@ -1,6 +1,17 @@
 var os = require('os');
 
-console.log(os.hostname());
-console.log(os.platform());
-console.log(os.arch());
-console.log(os.networkInterfaces());
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+    res.status(200).send({
+        hostname: os.hostname(),
+        platform: os.platform(),
+        arch: os.arch(),
+        networkInterfaces: os.networkInterfaces(),
+    });
+})
+
+app.listen(3000, function () {
+    console.log(`app listening on port ${3000}`)
+})
